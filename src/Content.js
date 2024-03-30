73989aac90698b10c07572715627827b1fde747a
Nathan Smith <nathanielvs@gmail.com>
2024-03-30 14:19:08 -0600

    Add different types of click events

--- src/Content.js
+++ src/Content.js
@@ -5,9 +5,27 @@ const Content = () => {
         return names[int];
     };
 
+    const handleClick = () => {
+        console.log("Simple Click");
+    };
+
+    const handleClick2 = (param) => {
+        console.log(`An ${param} was passed`);
+    };
+
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText);
+    };
+
     return (
         <main>
-            <p> Hello {handleNameChange()} </p>
+            <p on onDoubleClick={handleClick}>
+                {" "}
+                Hello {handleNameChange()}{" "}
+            </p>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => handleClick2("argument")}>Click Me 2</button>
+            <button onClick={(e) => handleClick3(e)}>Click Me 3</button>
         </main>
     );
 };
