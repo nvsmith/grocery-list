b80dc6c3b0cd449da0a0435c3f95261508eb0d3f
Nathan Smith <nathanielvs@gmail.com>
2024-04-03 14:46:50 -0600

    Add a call to API Request in the addItem function to create new items in the DB.

--- src/App.js
+++ src/App.js
@@ -37,11 +37,21 @@ function App() {
         }, 2000);
     }, []);
 
-    const addItem = (item) => {
+    const addItem = async (item) => {
         const id = items.length ? items[items.length - 1].id + 1 : 1;
         const myNewItem = { id, checked: false, item };
         const listItems = [...items, myNewItem];
         setItems(listItems);
+
+        const postOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(myNewItem),
+        };
+        const result = await apiRequest(API_URL, postOptions);
+        if (result) setFetchError(result);
     };
 
     const handleCheck = (id) => {
